7cf89cd3f40c6ee24f57e41548c9c0deecd1a789
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-03-12 23:35:26 +0600

    feat: Add AuditLog entity for tracking user activities

---
+++ src/main/java/com/example/springbootsecurityjwtauthenticationandauthorization/model/AuditLog.java
@@ -0,0 +1,27 @@
+package com.example.springbootsecurityjwtauthenticationandauthorization.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "audit_logs")
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String action;
+    private String performedBy;
+    private Instant timestamp;
+
+    @Column(columnDefinition = "TEXT")
+    private String details;
+}
