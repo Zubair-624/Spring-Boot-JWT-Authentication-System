2237324be2ea77b80cdccc31f4bd8b801c9d8138
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-03-14 23:37:30 +0600

    feat(security): Implement Permission entity for role-based access

---
+++ src/main/java/com/example/springbootsecurityjwtauthenticationandauthorization/model/Permission.java
@@ -0,0 +1,26 @@
+package com.example.springbootsecurityjwtauthenticationandauthorization.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "permission")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Column(nullable = false)
+    private String name;
+
+}
