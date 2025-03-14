f04d70b7548a2c74e04a155c28daa03ff01075b6
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-03-14 23:42:00 +0600

    feat(auth): Introduce BlacklistToken entity for secure session management

---
+++ src/main/java/com/example/springbootsecurityjwtauthenticationandauthorization/model/BlacklistToken.java
@@ -0,0 +1,27 @@
+package com.example.springbootsecurityjwtauthenticationandauthorization.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "blacklisted_toknes")
+public class BlacklistToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String token;
+
+    private Instant expiresAt;
+}
