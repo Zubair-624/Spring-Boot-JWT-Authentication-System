b3ab03c78f4c28058c08da84a645c80adff7f306
Zubair-624 <zubairmazumder624@gmail.com>
2025-03-15 23:59:39 +0600

    feat(dto): Implement RoleDTO for role-based access control

---
+++ src/main/java/com/example/springbootsecurityjwtauthenticationandauthorization/dto/RoleDTO.java
@@ -0,0 +1,15 @@
+package com.example.springbootsecurityjwtauthenticationandauthorization.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleDTO {
+
+    private String roleName;
+}
