05577715799273dae0a50888afdbcf0957f094c8
Zubair-624 <zubairmazumder624@gmail.com>
2025-03-15 23:58:27 +0600

    feat(dto): Add AuthResponseDTO for authentication response

---
+++ src/main/java/com/example/springbootsecurityjwtauthenticationandauthorization/dto/AuthResponseDTO.java
@@ -0,0 +1,17 @@
+package com.example.springbootsecurityjwtauthenticationandauthorization.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthResponseDTO {
+
+    private String accessToken;
+
+    private String refreshToken;
+}
