6acd4aedf564b564a793728304ec9b53d527aec9
Zubair-624 <zubairmazumder624@gmail.com>
2025-03-21 05:52:38 +0600

    feat(auth): Implement BlacklistTokenService for token security
    - Added blacklistToken method to revoke JWT tokens
    - Implemented isTokenBlacklisted method to check blacklisted tokens
    - Supports enhanced security by preventing token reuse

---
+++ src/main/java/com/example/springbootsecurityjwtauthenticationandauthorization/service/BlacklistTokenService.java
@@ -0,0 +1,24 @@
+package com.example.springbootsecurityjwtauthenticationandauthorization.service;
+
+import com.example.springbootsecurityjwtauthenticationandauthorization.model.BlacklistToken;
+import com.example.springbootsecurityjwtauthenticationandauthorization.repository.BlacklistTokenRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class BlacklistTokenService {
+
+    private final BlacklistTokenRepository blacklistTokenRepository;
+
+    public void blacklistToken(String token){
+        BlacklistToken blacklistToken = new BlacklistToken(token);
+        blacklistTokenRepository.save(blacklistToken);
+    }
+
+    public boolean isTokenBlacklisted(String token){
+        return blacklistTokenRepository.existsById(token);
+    }
+}
